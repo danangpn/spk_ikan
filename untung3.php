50e5cb68bd1e8651aa354b68ee0a847f44c50e64
danangpn <dpoerwo@gmail.com>
2015-06-03 03:12:07 +0700

    first commit

---
+++ untung3.php
@@ -0,0 +1,43 @@
+  <table width="100%" border="0">
+                <tr>
+                  <td width="46%">Lele</td>
+                  <td width="1%">:</td>
+                  <td width="53%"><font size="+0">Rp. <?php echo"$untungLele";?></font></td>
+                </tr>
+                <tr>
+                  <td>Nila</td>
+                  <td>:</td>
+                  <td><font size="+0">Rp. <?php echo"$untungNila";?></font></td>
+                </tr>
+                <tr>
+                  <td>Gurami</td>
+                  <td>:</td>
+                  <td><font size="+0">Rp. <?php echo"$untungGurami";?></font></td>
+                </tr>
+                <tr>
+                  <td>Karper</td>
+                  <td>:</td>
+                  <td><font size="+0">Rp. <?php echo"$untungKarper";?></font></td>
+                </tr>
+                <tr>
+                  <td>Patin</td>
+                  <td>:</td>
+                  <td><font size="+0">Rp. <?php echo"$untungPatin";?></font></td>
+                </tr>
+                <tr>
+                  <td>Bawal</td>
+                  <td>:</td>
+                  <td><font size="+0">Rp. <?php echo"$untungBawal";?></font></td>
+                </tr>
+                <tr><td colspan="3"><hr color="#DDDDDD"></td></tr>
+                <tr>
+                  <td>Keuntungan terbesar (<?php echo"$siProfit";?>)</td>
+                  <td>:</td>
+                  <td>Rp. <?php echo"$profit";?></td>
+                </tr>
+                <tr>
+                  <td>Keuntungan terkecil (<?php echo"$siProfitMin";?>)</td>
+                  <td>:</td>
+                  <td>Rp. <?php echo"$profitMin";?></td>
+                </tr>
+                </table>
